e8a2002adb661dd1446cc9ccc12389a5ea8b5b82
NKNZ <nikanozadze166@gmail.com>
2024-11-19 00:13:46 +0400

    Homework N20

---
+++ Homework_N20.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+endpoint = 'https://rickandmortyapi.com/api/character'
+
+response = requests.get(endpoint)
+page_quantity = response.json()['info']['pages']
+data = dict()
+
+for page in range(1, 15): # 15 გვერდის მერე მონაცემები აღარ მოაქვს სწორად, ლიმიტს აღწევს ჩემი აზრით
+    new_endpoint = f'{endpoint}?page={page}'
+    response = requests.get(new_endpoint)
+    characters = response.json()['results']
+    for character in characters:
+        data[character['name']] = []
+        for ep in character['episode']:
+            data[character['name']].append(ep.split('/')[-1])
+
+
+with open('file.json', 'w') as json_file:
+    json_file.write(json.dumps(data, indent=4))
+
